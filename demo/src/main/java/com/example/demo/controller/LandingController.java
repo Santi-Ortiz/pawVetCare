14a2b3e764df130cfdd3cdacf09828595af497e0
Alejandra Casas <141421692+al-calo@users.noreply.github.com>
2024-08-28 23:44:36 -0500

    Controller Landing y Boton PVC arreglado

--- demo/src/main/java/com/example/demo/controller/LandingController.java
+++ demo/src/main/java/com/example/demo/controller/LandingController.java
@@ -6,13 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/landing")
 public class LandingController {
     
-    // localhost:8090/home
+    // localhost:8090/landing/home
     @GetMapping("/home")
     public String home(){
         return "landingPage";
+    }
+
+    // localhost:8090/landing/contacto
+    @GetMapping("/contacto")
+    public String contacto(){
+        return "contacto";
+    }
+
+    // localhost:8090/landing/equipo
+    @GetMapping("/equipo")
+    public String equipo(){
+        return "equipo";
     }
 
     @GetMapping("/login")
