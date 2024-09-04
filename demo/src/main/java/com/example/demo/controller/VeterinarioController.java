fb89f8627dc6357ee5e8af3ab8bcb30163953ba3
Alejandra Casas <141421692+al-calo@users.noreply.github.com>
2024-09-04 17:04:35 -0500

    Veterinario casi completo falta en cliente Thymleaf

--- demo/src/main/java/com/example/demo/controller/VeterinarioController.java
+++ demo/src/main/java/com/example/demo/controller/VeterinarioController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entity.Cliente;
 import com.example.demo.entity.Mascota;
 import com.example.demo.service.AdminService;
+import com.example.demo.service.ClienteService;
 import com.example.demo.service.MascotaService;
 
 @Controller
@@ -20,6 +22,8 @@ public class VeterinarioController {
     private MascotaService MascotaService;
     @Autowired
     private AdminService adminService;
+    @Autowired
+    private ClienteService clientService;
     
     @GetMapping("/mascotas")
      public String mostrarMascotas(Model model){
@@ -50,5 +54,27 @@ public class VeterinarioController {
     public String redirectToMascota(@RequestParam("id") Long id) {
         return "redirect:/veterinario/mascotas/" + id;
  
+    }
+
+    // http://localhost:8090/veterinario/clientes
+    @GetMapping("/clientes")
+    public String mostrarClientesAdmin(Model model){
+        model.addAttribute("clientes", adminService.SearchAllClients());
+        model.addAttribute("cliente", new Cliente());
+        return "vet_mostrarTodosClientes";
+    }
+
+    // http://localhost:8090/admin/cliente/{id}
+    @GetMapping("/cliente/{cedula}")
+    public String mostrarCliente(Model model, @PathVariable("cedula") Long cedula) {
+        Cliente cliente = clientService.obtenerClientePorCedula(cedula);
+
+        if(cliente != null){
+           model.addAttribute("cliente", cliente); 
+        } else {
+           throw new NotPetFoundException(cedula);
+        }
+
+        return "vet_MostrarInfoCliente";
     }
 }
