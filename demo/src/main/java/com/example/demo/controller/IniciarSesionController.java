9cb62b6436c5557f2acbd03b4979954193d8134a
Alejandra Casas <141421692+al-calo@users.noreply.github.com>
2024-10-06 21:23:33 -0500

    Rest 1.0

--- demo/src/main/java/com/example/demo/controller/IniciarSesionController.java
+++ demo/src/main/java/com/example/demo/controller/IniciarSesionController.java
@@ -3,12 +3,15 @@ package com.example.demo.controller;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Cliente;
 import com.example.demo.entity.Veterinario;
@@ -19,7 +22,7 @@ import com.example.demo.service.VeterinarioService;
 
 import jakarta.servlet.http.HttpSession;
 
-@Controller
+/*@Controller
 @RequestMapping("/iniciarSesion")
 public class IniciarSesionController {
     
@@ -118,4 +121,72 @@ public class IniciarSesionController {
         
         return "iniciarSesion";
     }
+}*/
+@RestController
+@RequestMapping("/api/sesion")
+public class IniciarSesionController {
+    
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private VeterinarioService vetService;
+    
+    // localhost:8090/api/sesion/cliente
+    @PostMapping("/cliente")
+    public ResponseEntity<?> loginCliente(@RequestParam("id") Long id, HttpSession session) {
+        try {
+            Cliente cliente = clienteService.obtenerCliente(id);
+            session.setAttribute("cliente", cliente);
+            return ResponseEntity.ok(cliente);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encontrado.");
+        }
+    }
+
+    // http://localhost:8090/api/sesion/cliente/inicio
+    @GetMapping("/cliente/inicio")
+    public ResponseEntity<?> inicioCliente(HttpSession session) { 
+        Cliente cliente = (Cliente) session.getAttribute("cliente");
+        if (cliente != null) {
+            return ResponseEntity.ok(cliente); 
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("No hay sesión activa.");
+    }
+
+    // http://localhost:8090/api/sesion/veterinario
+    @PostMapping("/veterinario")
+    public ResponseEntity<?> sesionVet(@RequestParam("cedula") Long cedula, @RequestParam("password") String password, HttpSession session){
+        try {
+            Veterinario vet = vetService.buscarVet(cedula);
+            if (vet.getContrasena().equals(password)) {
+                session.setAttribute("vet", vet);
+                return ResponseEntity.ok(vet); 
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Contraseña incorrecta.");
+            }
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Veterinario no encontrado.");
+        }
+    }
+
+    // http://localhost:8090/api/sesion/admin
+    @PostMapping("/admin")
+    public ResponseEntity<?> sesionAdmin(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session){
+        try {
+            Admin admin = adminService.findByUsuario(username);
+            if (admin.getContrasena().equals(password)) {
+                session.setAttribute("admin", admin);
+                return ResponseEntity.ok(admin); 
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Contraseña incorrecta.");
+            }
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Admin no encontrado.");
+        }
+    }
 }
+
