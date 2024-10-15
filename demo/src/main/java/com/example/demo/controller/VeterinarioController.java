d9e67d6f37f6a1b189d6ea663ca5ea3f42ce91c6
Santi-Ortiz <106709063+Santi-Ortiz@users.noreply.github.com>
2024-10-14 23:52:29 -0500

    Agregar Veterinario CHECK

--- demo/src/main/java/com/example/demo/controller/VeterinarioController.java
+++ demo/src/main/java/com/example/demo/controller/VeterinarioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -196,7 +197,7 @@ public class VeterinarioController {
     }
 
     // Se agrega un veterinario nuevo
-    @GetMapping("/agregar")
+    @PostMapping("/agregar")
     public ResponseEntity<Veterinario> agregarVet(@RequestBody Veterinario veterinario) {
         veterinarioService.agregarVet(veterinario);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
