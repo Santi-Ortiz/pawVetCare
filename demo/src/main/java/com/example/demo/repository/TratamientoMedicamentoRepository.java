cad9dc3befec2c4862b2d60a65336797eeb28149
Santi-Ortiz <106709063+Santi-Ortiz@users.noreply.github.com>
2024-10-17 00:01:59 -0500

    Cambio en repositorio tratamientoMedicamento

--- demo/src/main/java/com/example/demo/repository/TratamientoMedicamentoRepository.java
+++ demo/src/main/java/com/example/demo/repository/TratamientoMedicamentoRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.TratamientoMedicamento;
 
 @Repository
-public class TratamientoMedicamentoRepository extends JpaRepository<TratamientoMedicamento, Long> {
+public interface TratamientoMedicamentoRepository extends JpaRepository<TratamientoMedicamento, Long> {
   
 }
