2acd81574a966235911738cc819215404a3e6db7
Santi-Ortiz <106709063+Santi-Ortiz@users.noreply.github.com>
2024-10-23 23:22:18 -0500

    pruebas de machetazos funcionando

--- demo/src/test/java/com/example/demo/service/TratamientoServiceTestNaive.java
+++ demo/src/test/java/com/example/demo/service/TratamientoServiceTestNaive.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,7 +42,7 @@ public class TratamientoServiceTestNaive {
     private TratamientoRepository tratamientoRepository;
 
     @Autowired
-    private TratamientoMedicamentoRepository  tratamientoMedicamentoRepository;
+    private TratamientoMedicamentoRepository tratamientoMedicamentoRepository;
 
     @Autowired
     private MedicamentoRepository medicamentoRepository;
@@ -54,9 +58,8 @@ public class TratamientoServiceTestNaive {
 
     @Test
     public void TratamientoService_agregarTratamiento_Tratamiento() {
-        Especialidad especialidad = new Especialidad("cirugia");
-        especialidadRepository.save(especialidad); 
-        Veterinario veterinario = new Veterinario(789456L, "2345", "foto", "Erick Prydz", true, especialidad);
+
+        Veterinario veterinario = new Veterinario(789456L, "2345", "foto", "Erick Prydz", true, null);
         veterinarioRepository.save(veterinario);
         Mascota mascota = new Mascota("Tobias", "Bulldog", 2, 10.5f, "pulgas", "foto", true, null);
         mascotaRepository.save(mascota);
@@ -70,18 +73,21 @@ public class TratamientoServiceTestNaive {
 
         Assertions.assertThat(nuevoTratamiento).isNotNull();
         Assertions.assertThat(nuevoTratamiento.getFecha()).isEqualTo(java.sql.Date.valueOf("2024-10-10"));
-        //Assertions.assertThat(nuevoTratamiento.getVeterinario()).isEqualTo(veterinario);
     }
 
     @Test
     public void TratamientoService_findById_Tratamiento() {
 
-        Veterinario veterinario = new Veterinario(123456L, "2345", "foto", "Pablo Perez", true,
-                new Especialidad("Traumatologia"));
+        Veterinario veterinario = new Veterinario(123456L, "2345", "foto", "Pablo Perez", true, null);
+        veterinarioRepository.save(veterinario);
+
         Mascota mascota = new Mascota("Firulais", "Bulldog", 2, 10.5f, "Deficiencia renal", "foto", true, null);
+        mascotaRepository.save(mascota);
         Tratamiento tratamiento = new Tratamiento(java.sql.Date.valueOf("2024-09-10"), veterinario, mascota);
+        tratamientoRepository.save(tratamiento);
 
         Tratamiento tratamientoBuscado = tratamientoService.findById(tratamiento.getId());
+        tratamientoRepository.save(tratamiento);
 
         Assertions.assertThat(tratamientoBuscado).isNotNull();
         Assertions.assertThat(tratamientoBuscado.getFecha()).isEqualTo(java.sql.Date.valueOf("2024-09-10"));
@@ -89,11 +95,12 @@ public class TratamientoServiceTestNaive {
 
     @Test
     public void TratamientoService_findAll_NoEmptyList() {
-
-        Veterinario veterinario = new Veterinario(123456L, "2345", "foto", "Pablo Perez", true,
-                new Especialidad("Traumatologia"));
+        Veterinario veterinario = new Veterinario(9999999L, "2345", "foto", "Pablo Perez", true, null);
+        veterinarioRepository.save(veterinario);
         Mascota mascota = new Mascota("Firulais", "Bulldog", 2, 10.5f, "Deficiencia renal", "foto", true, null);
+        mascotaRepository.save(mascota);
         Tratamiento tratamiento = new Tratamiento(java.sql.Date.valueOf("2024-09-10"), veterinario, mascota);
+        tratamientoRepository.save(tratamiento);
 
         tratamientoService.agregarTratamiento(tratamiento, new Medicamento("Genoprasol", 20.5, 25.0, 10, 15), 5);
 
@@ -105,27 +112,32 @@ public class TratamientoServiceTestNaive {
 
         Tratamiento tratamiento = new Tratamiento();
         tratamiento.setId(1L);
+        tratamientoRepository.save(tratamiento);
 
         tratamientoService.eliminarTratamiento(1L);
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            tratamientoService.findById(1L);
+        });
 
-        Assertions.assertThat(tratamiento).isNull();
+        Assertions.assertThat(exception.getMessage()).isEqualTo("Tratamiento no encontrado");
     }
 
     @Test
     public void TratamientoService_obtenerTotalTratamientosMesActual() {
 
-        Tratamiento tratamiento1 = new Tratamiento(java.sql.Date.valueOf("2024-10-01"), new Veterinario(),
-                new Mascota());
-        Tratamiento tratamiento2 = new Tratamiento(java.sql.Date.valueOf("2024-10-15"), new Veterinario(),
-                new Mascota());
-        Tratamiento tratamiento3 = new Tratamiento(java.sql.Date.valueOf("2024-10-18"), new Veterinario(),
-                new Mascota());
-        Tratamiento tratamiento4 = new Tratamiento(java.sql.Date.valueOf("2024-10-20"), new Veterinario(),
-                new Mascota());
-        tratamientoService.agregarTratamiento(tratamiento1, null, 0);
-        tratamientoService.agregarTratamiento(tratamiento2, null, 0);
-        tratamientoService.agregarTratamiento(tratamiento3, null, 0);
-        tratamientoService.agregarTratamiento(tratamiento4, null, 0);
+        Tratamiento tratamiento1 = new Tratamiento(java.sql.Date.valueOf("2024-10-01"), null,
+                null);
+        tratamientoRepository.save(tratamiento1);
+        Tratamiento tratamiento2 = new Tratamiento(java.sql.Date.valueOf("2024-10-15"), null, null);
+        tratamientoRepository.save(tratamiento2);
+        Tratamiento tratamiento3 = new Tratamiento(java.sql.Date.valueOf("2024-10-18"), null,null);
+        tratamientoRepository.save(tratamiento3); 
+
+        Medicamento medicamento = new Medicamento("Genoprasol", 20.5, 25.0, 10, 15);
+
+        tratamientoService.agregarTratamiento(tratamiento1, medicamento, 0);
+        tratamientoService.agregarTratamiento(tratamiento2, medicamento, 0);
+        tratamientoService.agregarTratamiento(tratamiento3, medicamento, 0);
 
         long totalTratamientos = tratamientoService.obtenerTotalTratamientosMesActual();
 
@@ -134,36 +146,17 @@ public class TratamientoServiceTestNaive {
     }
 
     @Test
-    public void TratamientoService_obtenerTop3Tratamientos() {
-        // Arrange
-        Tratamiento t1 = new Tratamiento();
-        t1.setId(1L);
-
-        Tratamiento t2 = new Tratamiento();
-        t2.setId(2L);
-
-        Tratamiento t3 = new Tratamiento();
-        t3.setId(3L);
-
-        Tratamiento t4 = new Tratamiento();
-        t4.setId(4L);
-
-        List<Tratamiento> top3 = tratamientoService.obtenerTop3Tratamientos();
-
-        Assertions.assertThat(top3).isNotEmpty();
-        Assertions.assertThat(top3.size()).isEqualTo(3);
-        Assertions.assertThat(top3.size()).isGreaterThan(0);
-    }
-
-    @Test
     public void TratamientoService_ErrorSinSuficientesUnidades() {
         Tratamiento tratamiento = new Tratamiento();
         Medicamento medicamento = new Medicamento("Antibiótico A", 25.5D, 20.5D, 5, 50); // Solo 5 unidades disponibles
         int cantidad = 10; // Se solicita más de lo disponible
 
-        tratamientoService.agregarTratamiento(tratamiento, medicamento, cantidad);
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            tratamientoService.agregarTratamiento(tratamiento, medicamento, cantidad);
+        });
 
-        Assertions.assertThat(tratamientoService.obtenerTratamientos()).isEmpty();
+        Assertions.assertThat(exception.getMessage()).isEqualTo("No hay suficientes unidades del medicamento Antibiótico A");
     }
+                
 
 }
