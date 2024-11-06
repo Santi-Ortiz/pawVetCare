fc9c8dedfb7b00c38418a1cec92420643c274f01
Santi-Ortiz <106709063+Santi-Ortiz@users.noreply.github.com>
2024-11-05 19:08:21 -0500

    Cambios en DatabaseInit con Lombok

--- demo/src/main/java/com/example/demo/entity/Veterinario.java
+++ demo/src/main/java/com/example/demo/entity/Veterinario.java
@@ -7,10 +7,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
